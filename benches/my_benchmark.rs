dadf6afc2c9c8b08400c5207a9b49e230091c238
aljo242 <amj92@pitt.edu>
2021-05-24 12:10:37 -0400

    added basic criterion bench

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,16 +1,11 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
+use subspace_assignment::run;
 
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n - 1) + fibonacci(n - 2),
-    }
-}
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+    c.bench_function("256 bit", |b| b.iter(|| run()));
 }
+
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
