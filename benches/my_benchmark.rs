bcb16ed785dc74fb585a3fb446ac56da7db650ab
aljo242 <amj92@pitt.edu>
2021-05-27 15:30:10 -0400

    benches update

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,11 +1,34 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use subspace_assignment::run;
+use subspace_assignment::{inverse_sqrt, sqrt_permutation, run, from_block, gen_largest_prime, PRIME_BYTE_SIZE, Block};
+use rug::{Integer};
 
+fn encode_decode(c: &mut Criterion) {
+    let prime = gen_largest_prime(PRIME_BYTE_SIZE);
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("256_bit", |b| b.iter(|| run()));
+    // e = (p + 1) / 4
+    let mut exponent = prime.clone() + Integer::from(1);
+    exponent.div_exact_u_mut(4);
+
+    let block_in: Block = rand::random();
+    let int = from_block(block_in);
+
+    c.bench_function("encode", |b| b.iter(|| 
+        sqrt_permutation(&int, &exponent, &prime)    
+    ));
+
+    let perm = sqrt_permutation(&int, &exponent, &prime); 
+
+    c.bench_function("decode", |b| b.iter(|| 
+        inverse_sqrt(&perm, &prime)
+    ));
+
+}
+
+fn end_to_end(c: &mut Criterion) {
+    c.bench_function("end_to_end", |b| b.iter(|| run()));
 }
 
 
-criterion_group!(benches, criterion_benchmark);
+
+criterion_group!(benches, end_to_end, encode_decode);
 criterion_main!(benches);
